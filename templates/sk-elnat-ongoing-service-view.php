ed9ab3ea8fe5faea53e1247dee3e767571b01f12
Joakim Sundqvist <joakim.sundqvist@cybercom.com>
2018-08-14 15:50:48 +0200

    Corrected style according to customers need

--- templates/sk-elnat-ongoing-service-view.php
+++ templates/sk-elnat-ongoing-service-view.php
@@ -3,8 +3,8 @@
         <i class="sk-elnat-icon-32 material-icons">warning</i>
     </div>
     <div class="sk-elnat-info-container">
-        <small><?php echo $value['area']; ?> - Pågående t.o.m. <?php echo $value['estendtime']; ?></small>
-        <span class="sk-elnat-info-text"><?php echo $value['outagedescr']; ?></span>
+        <b><?php echo $value['area']; ?></b><small> Startade <?php echo $value['plannedstarttime']; ?>, beräknas åtgärdat/åtgärdades <?php echo $value['estendtime']; ?></small>
+        <small class="sk-elnat-info-text"><?php echo $value['outagedescr']; ?></small>
     </div>
 </div>
 
--- templates/sk-elnat-planned-service-view.php
+++ templates/sk-elnat-planned-service-view.php
@@ -1,9 +1,9 @@
 <div class="sk-elnat-service-message-container">
     <div class="sk-elnat-icon-container">
-        <i class="sk-elnat-icon-32 material-icons">error</i>
+        <i class="sk-elnat-icon-32 material-icons">calendar_today</i>
     </div>
     <div class="sk-elnat-info-container">
-        <small><?php echo $value['area']; ?> - Planerad fr.o.m. <?php echo $value['plannedstarttime']; ?></small>
-        <span class="sk-elnat-info-text"><?php echo $value['outagedescr']; ?></span>
+        <b><?php echo $value['area']; ?></b><small> Planerad <?php echo $value['plannedstarttime']; ?> - <?php echo $value['plannedstarttime']; ?></small>
+        <small class="sk-elnat-info-text"><?php echo $value['outagedescr']; ?></small>
     </div>
 </div>
