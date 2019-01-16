5515fc4f670beaf74f79bdfe973771669e613e49
Joakim Sundqvist <joakim.sundqvist@cybercom.com>
2019-01-16 11:27:00 +0100

    Added code so that the ongoing outages is only displayed if there are any, and also some small styling tweeks

--- templates/sk-elnat-service-messages-iterator.php
+++ templates/sk-elnat-service-messages-iterator.php
@@ -34,9 +34,12 @@ usort($finnished_jobs, function ($a, $b)
     if($a['endtime'] == $b['endtime']){ return 0 ; }
 	return ($a['endtime'] > $b['endtime']) ? -1 : 1;
 });
+//$current_jobs = [];
+$has_current_jobs = (sizeof($current_jobs) > 0);
 ?>
 
 <div class="row">
+<?php if ($has_current_jobs) : ?>
     <div class="col-lg-6 sk-elnat-col">
         <a class="sk-elnat-toggle" data-toggle="collapse" href="#collapseCurrent" role="button" aria-expanded="false" aria-controls="collapseCurrent">
             <div class="sk-elnat-warning no-transition alert alert-warning" role="alert">
@@ -54,19 +57,20 @@ usort($finnished_jobs, function ($a, $b)
             <div class="no-transition alert alert-warning" role="alert">
 
                 <?php foreach( $current_jobs as $key => $value ): ?>
-                    <?php if ( $value['area'] != "Icke definierad" ) echo '<b>' . $value['area'] . '</b>'; ?>
-                    <p data-id="<?php echo $value['id']; ?>" class="no-transition">
+                    <?php if ( $value['area'] != "Icke definierad" ) echo '<b style="display: block;">' . $value['area'] . '</b>'; ?>
+                    <p data-id="<?php echo $value['id']; ?>" class="no-transition" style="display: inline-block;">
                         <i class="material-icons" style="vertical-align: middle; margin-top: -3px; margin-right: .2rem;">
                             <?php echo ($value['plannedjob'] ? 'error' : 'warning'); ?>
                         </i>
                         <?php if ( $value['area'] != "Icke definierad" ) : ?>
-                            <?php echo ($value['plannedjob'] ? 'Strömavbrott i samband med planerat arbete startade:<br>' : 'Strömavbrott startade:<br>') . date("Y-m-d H:i", strtotime($value['starttime'])); ?>
+                            <?php echo ($value['plannedjob'] ? 'Strömavbrott i samband med planerat arbete startade: ' : 'Strömavbrott startade: ') ; ?>
                         <?php else : ?>
                             Ett strömavbrott har uppstått. 
                         <?php endif; ?>
                     </p>
+                    <small style="display: inline-block;"><?php echo date("Y-m-d H:i", strtotime($value['starttime'])); ?></small>
 
-                    <small>
+                    <small style="display: block;">
                         <?php 
                             if ( $value['area'] == "Icke definierad" ) echo "Vi felsöker orsaken och vilket geografiskt område som är påverkat. ";
                             if ( $value['statusinfo'] != 'Ej definerad' ) echo $value['statusinfo'] . ". ";
@@ -83,14 +87,15 @@ usort($finnished_jobs, function ($a, $b)
             </div>
         </div>
     </div>
-    <div class="col-lg-6 sk-elnat-col">
+<?php endif; ?>
+    <div class="<?php echo $has_current_jobs ? 'col-lg-6' : 'col-lg-12'; ?> sk-elnat-col">
         <a class="sk-elnat-toggle" data-toggle="collapse" href="#collapseDone" role="button" aria-expanded="false" aria-controls="collapseDone">
             <div class="sk-elnat-success no-transition alert alert-success" role="alert">
                 <p class="no-transition" style="vertical-align: middle; display: inline-flex; line-height: 33px;">
                     <i style="padding-right: .3rem;" class="sk-elnat-icon-32 material-icons">
                         check_circle_outline
                     </i>
-                    <b>Nyligen åtgärdade strömavbrott</b>
+                    <b><?php echo $has_current_jobs ? 'Nyligen åtgärdade strömavbrott' : 'Vi har för tillfället inga strömavbrott'; ?></b>
                 </p>
                 <i style="float: right; color: inherit;" class="sk-elnat-icon-32 material-icons">keyboard_arrow_down</i>
             </div> 
@@ -100,15 +105,17 @@ usort($finnished_jobs, function ($a, $b)
             <div class="sk-elnat-alert alert alert-success" role="alert">
 
                 <?php foreach( $finnished_jobs as $key => $value ): ?>
-                    <?php if ( $value['area'] != "Icke definierad" ) echo '<b>' . $value['area'] . '</b>'; ?>
-                    <p data-id="<?php echo $value['id']; ?>" class="no-transition">
+                    <?php if ( $value['area'] != "Icke definierad" ) echo '<b style="display: block;">' . $value['area'] . '</b>'; ?>
+                    <p data-id="<?php echo $value['id']; ?>" class="no-transition" style="display: inline-block;">
                         <i class="material-icons" style="vertical-align: middle; margin-top: -3px; margin-right: .2rem;">
                             check_circle
                         </i>
-                        <?php echo ($value['plannedjob'] ? 'Strömavbrott i samband med planerat arbete pågick:<br>' : 'Strömavbrott pågick:<br>') . date("Y-m-d H:i", strtotime($value['starttime'])) . ' - ' . date("Y-m-d H:i", strtotime($value['endtime'])); ?>
+                        <?php echo ($value['plannedjob'] ? 'Strömavbrott i samband med planerat arbete pågick:<br>' : 'Strömavbrott pågick:<br>'); ?>
                     </p>
 
-                    <small>
+                    <small style="display: inline-block;"><?php echo date("Y-m-d H:i", strtotime($value['starttime'])) . ' - ' . date("Y-m-d H:i", strtotime($value['endtime'])); ?></small>
+
+                    <small style="display: block;">
                         <?php
                             if ( $value['statusinfo'] != 'Ej definerad' ) echo $value['statusinfo'] . ". ";
                             if ( $value['statusinfo2'] != 'Meddelas senare' ) echo $value['statusinfo2'] . ". ";
--- templates/sk-elnat-service-messages-view.php
+++ templates/sk-elnat-service-messages-view.php
@@ -1,11 +1,13 @@
 <?php if( $service_messages ): ?>
     <div class="sk-elnat-service-messages">
         <?php include( plugin_dir_path(__DIR__) . 'templates/sk-elnat-service-messages-iterator.php' ); ?>
-        <div style="margin-top: 1rem;"class="row">
-            <div class="col-lg-12">
-                <h3><a style="color:#e5312b;" href="http://avbrottskarta.sundsvallelnat.se/" target="_blank">Besök vår avbrottskarta </a> för geografisk översikt över pågående avbrott och planerade arbeten.</h3>
+        <?php if ($has_current_jobs) : ?>
+            <div style="margin-top: 1rem;"class="row">
+                <div class="col-lg-12">
+                    <h3><a style="color:#e5312b;" href="http://avbrottskarta.sundsvallelnat.se/" target="_blank">Besök vår avbrottskarta </a> för geografisk översikt över pågående avbrott och planerade arbeten.</h3>
+                </div>
             </div>
-        </div>
+        <?php endif; ?>
     </div>
 <?php else: ?>
     <div class="alert alert-danger" role="alert">
